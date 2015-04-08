0ab15964895e44a67f1fcc5bb21236e36f536654
laursuom <laur1.suomalainen@gmail.com>
2015-04-08 13:03:51 +0300

    Added a shutdown hook for killing the running containers

--- src/main/java/com/mycompany/dockerd/DockerD.java
+++ src/main/java/com/mycompany/dockerd/DockerD.java
@@ -1,43 +1,54 @@
 package com.mycompany.dockerd;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
-
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Scanner;
-        /**
-         *
-         * @author Lauri Suomalainen
-         */
-
-
+/**
+ *
+ * @author Lauri Suomalainen
+ */
 public class DockerD {
 
     /**
      * @param args the command line arguments
      */
-    
-    public static void main(String[] args) throws InstantiationException, IllegalAccessException {
-        
-        int port;
+    public static void main(String[] args) {
+
         if (args.length == 0) {
             System.err.println("Usage: DockerD <portnumber>");
             System.exit(0);
         } else {
-//            File portFile = new File(args[0]);
+
+            //            File portFile = new File(args[0]);
 //            if (portFile.exists()) {
 //                ports = ArgParserService.parsePortsFromFile(portFile);
 //            } else {
 //                ports = ArgParserService.parsePortsFromArgumentList(args);
 //           }
-             ConnectionListener listener = new ConnectionListener(Integer.parseInt(args[0]), new DefaultCommands()); 
-             listener.run();
+            Runtime.getRuntime().addShutdownHook(new Thread() {
+                @Override
+                public void run() {
+                    String[] cmd = {
+                        "/bin/sh",
+                        "-c",
+                        "docker kill $(docker ps -q)"
+                    };
+
+                    try {
+                        Runtime.getRuntime().exec(cmd);
+                    } catch (IOException ex) {
+                        Logger.getLogger(DockerD.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
+            });
+            ConnectionListener listener = new ConnectionListener(Integer.parseInt(args[0]), new DefaultCommands());
+            listener.run();
         }
 
     }
